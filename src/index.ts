ec264d9de4316fc17a469092d0b21138812f2e81
Amit Kumar <amitdnagar4@gmail.com>
2024-12-03 14:01:42 +0530

    added cors and modified vercel.json and package.json file for deployment

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,16 @@ import connectDB from './config/db';
 import { notFound, errorHandler } from './middleware/errormiddleware';
 import userRoutes from '../src/routes/userRoutes';
 import todoRoutes from '../src/routes/todoRoutes';
-
+import { Request, Response } from 'express';
+import cors from 'cors';
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Server is running.' });
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/todos', todoRoutes);
